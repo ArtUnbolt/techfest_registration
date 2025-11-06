bcfac76a0e8b7a350dba081f17ec37b8fd542c66
Yangzon <12412839@benilde.edu.ph>
2025-11-06 12:45:25 +0800

    Implemented participant name and track input and display

--- yangzon_techfest_registration.py
+++ yangzon_techfest_registration.py
@@ -8,5 +8,21 @@ while participants <= 0:
         print("Invalid number of participants.")
         break
 
+a = set()
+b = set()
+records = {
+"name": a,
+"track": b
+}
+
 if participants > 0:
-    print()
+    for i in range(participants):
+        name = input("Enter participant name: ")
+        a.add(name)
+        track = input("Enter chosen track: ")
+        b.add(track)
+
+print("Registered participants:")
+
+for i in range (0, participants):
+    print(f"{i + 1}. {list(a)[i]} - {list(b)[i]}")
